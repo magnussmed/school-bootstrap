8c069577dcdbf39e569d8311d1984c9530464c55
Magnus Smed <magnussmed1@gmail.com>
2020-03-02 17:16:49 +0100

    rewrited structure, issue on line 19. the line works seperate tho

--- Image/point.py
+++ Image/point.py
@@ -2,53 +2,50 @@ from PIL import Image
 import multiprocessing as mp
 import os
 
-def image() :
-	start = os.getcwd()
-	img = Image.open( "assets/img/3.jpg" )
-	width, height = img.size
+class Point( object ) :
+	def __init__( self ) :
+		self.photo = Image.open( "assets/img/3.jpg" )
+		self.width, self.height = self.photo.size
 
-	return [width, height, img]
+		mp.Process( target = self.bottom_middle ).start()
+		mp.Process( target = self.middle_top ).start()
 
-def bottom_middle() :
-	photo = image()
-	width = int(photo[0])
-	height = int(photo[1]/2)
+	def bottom_middle( self ) :
+		width = int( self.width )
+		height = int( self.height / 2 )
 
-	for x in range( 0, width ) :
-		for y in range( 0, height ) :
-			r, g, b = photo[2].getpixel((x, y))
-			average = int(sum([r, g, b])/3)
-			if average < 50 :
-				print(r,g,b)
-				print(x,y)
-				break
-			print( "Bottom-middle average: {} ({},{},{})".format(average, r, g, b) )
-			print( "Bottom-middle: {}, {}".format(x, y) )
-		else :
-			continue
-		break
+		for x in range( 0, width ) :
+			for y in range( 0, height ) :
+				print(self.photo.getpixel(( 500, 500 )))
+				average = int( sum([r, g, b]) / 3 )
+				if average < 50 :
+					print(r,g,b)
+					print(x,y)
+					break
+				print( "Bottom-middle average: {} ({},{},{})".format(average, r, g, b) )
+				print( "Bottom-middle: {}, {}".format(x, y) )
+			else :
+				continue
+			break
 
-def middle_top() :
-	photo = image()
-	width = int(photo[0])
-	height = int(photo[1]/2)
+	def middle_top( self ) :
+		width = int( self.width )
+		height = int( self.height / 2 )
 
-	for x in range( 0, width ) :
-		for y in range( height, photo[1] ) :
-			r, g, b = photo[2].getpixel((x, y))
-			average = int(sum([r, g, b])/3)
-			if average < 50 :
-				print(r,g,b)
-				print(x,y)
-				break
-			print( "Middle-top average: {} ({},{},{})".format(average, r, g, b) )
-			print( "Middle-top: {}, {}".format(x, y) )
-		else :
-			continue
-		break
+		for x in range( 0, width ) :
+			for y in range( height, self.height ) :
+				r, g, b = self.photo.getpixel(( x, y ))
+				average = int( sum([r, g, b]) / 3 )
+				if average < 50 :
+					print(r,g,b)
+					print(x,y)
+					break
+				#print( "Middle-top average: {} ({},{},{})".format(average, r, g, b) )
+				#print( "Middle-top: {}, {}".format(x, y) )
+			else :
+				continue
+			break
 
-if __name__=='__main__':
-	p1 = mp.Process( target = bottom_middle )
-	p2 = mp.Process( target = middle_top )
-	p1.start()
-	p2.start()
+
+if __name__ == '__main__' :
+	point = Point()
