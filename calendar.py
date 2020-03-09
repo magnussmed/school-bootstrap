4a7740809a6afada5ffd96b8afbbaff234fb6b39
Magnus Smed <magnussmed1@gmail.com>
2020-03-09 19:47:01 +0100

    works

--- calendar.py
+++ calendar.py
@@ -16,50 +16,53 @@ class Calendar( object ) :
 		# Sample output:
 		# [['11:30', '12:00'], ['15:00', '16:00'], ['18:00', '18:30']]
 
-		result = self.availability()
+		a = self.availability()
+		p = self.compare( a )
+
+		print( p )
 
 	def compare( self, a ) :
-		fr = []
-		for i in a[0] :
-			print(i)
-			fr.append(self.realtime( i, i[+1] ))
+		t = []
+		for a, b, c, d in zip( a[0], a[1], a[2], a[3] ):
+			diff = b[1] - d[0]
+			if diff >= self.duration :
+				t += c[0], a[1]
 
-		print(fr)
+		return t
 
 	def availability( self ) :
-
 		# For each current meeting --> get end time and start time from next in loop
 		first = []
+		firstReal = []
 		for c, n in zip( self.firstC, self.firstC[1:] ) :
 			if c[1] != n[0] :
-				diff = self.realtime( c[1], n[0] )
+				rt = self.realtime( c[1], n[0] )
+				diff = rt[1] - rt[0]
 				if diff >= self.duration :
 					first.append( [c[1], n[0]] )
-
-		#print( "Ledige tidsrum first: {}".format( first ) )
+					firstReal.append( [rt[0], rt[1]] )
 
 		# For each current meeting --> get end time and start time from next in loop
 		second = []
+		secondReal = []
 		for c, n in zip( self.secondC, self.secondC[1:] ) :
 			if c[1] != n[0] :
-				diff = self.realtime( c[1], n[0] )
+				rt = self.realtime( c[1], n[0] )
+				diff = rt[1] - rt[0]
 				if diff >= self.duration :
 					second.append( [c[1], n[0]] )
+					secondReal.append( [rt[0], rt[1]] )
 
-		#print("Ledige tidsrum second: {}".format(second))
-
-		return first, second
-
+		return first, firstReal, second, secondReal
 
 	def realtime( self, c, n ) :
-
 		start = c.split(':')
 		end = n.split(':')
 
-		start = (int(start[0]) * 60) + int(start[1])
-		end = (int(end[0]) * 60) + int(end[1])
+		start = ( int( start[0] ) * 60 ) + int( start[1] )
+		end = ( int( end[0] ) * 60 ) + int( end[1] )
 
-		return end - start
+		return start, end
 
 if __name__ == '__main__' :
 	point = Calendar()
